bdf3a31dd3f971d604f84d66067c80cb8a804d1c
Nelson Felipe <nelsonfelipeandradearaujo.@gmail.com>
2023-10-31 23:35:26 -0300

    nova versão

--- petCalendario.py
+++ petCalendario.py
@@ -2,112 +2,125 @@ import tkinter as tk
 from tkinter import ttk
 import calendar
 import datetime
+from ttkthemes import ThemedStyle
 
-def mostrar_calendario(year, month, root):
+year = datetime.datetime.now().year
+month = datetime.datetime.now().month
+day = datetime.datetime.now().day
+selected_day = None
 
-    def selecionar_dia(day):
-        selected_date.set(f"{month_names[month_var.get() - 1]} {day}, {year_var.get()}")
 
-    def atualizar_calendario():
-        cal = calendar.monthcalendar(year_var.get(), month_var.get())
-        clear_calendar()
-        row = 0
-        col = 0
-        today = datetime.datetime.now().day
-        for day_name in day_names:
-            day_label = tk.Label(frame, text=day_name, width=4, height=2, borderwidth=1, relief="solid")
-            day_label.grid(row=row, column=col, padx=2, pady=2)
-            col += 1
-        for week in cal:
-            row += 1
-            col = 0
-            for day in week:
-                if day == 0:
-                    day_label = tk.Label(frame, text="", width=4, height=2, borderwidth=1, relief="solid")
+def calendario(windows,nextimg,pastimg):
+
+
+    def change_month(val):
+        global month
+        global year
+        month += val
+        if month < 1:
+            month = 12
+            year -= 1
+        elif month > 12:
+            month = 1
+            year += 1
+        label_h.config(text=months_pt[month-1])
+        year_label.config(text=year)
+        update_days()
+
+    def select_day(button):
+        global selected_day
+        if selected_day:
+            selected_day.config(bg='white')
+        selected_day = button
+        selected_day.config(bg='lightblue')
+
+    def update_days():
+        # Obter o número de dias no mês e o dia da semana do primeiro dia do mês
+        num_dias = calendar.monthrange(year, month)[1]
+        dia_semana = calendar.weekday(year, month, 1)
+
+        # Ajustar para que domingo seja 0, segunda-feira seja 1, etc.
+        dia_semana = (dia_semana + 1) % 7
+
+        # Atualizar os números dos dias na interface
+        num = 1
+        for x in range(3, 9):
+            for y in range(7):
+                if x == 3 and y < dia_semana:
+                    label_num = tk.Label(frame, text='', width=5, height=2, borderwidth=1, relief="solid", background='gray')
+                elif num == day and month == datetime.datetime.now().month and year == datetime.datetime.now().year:
+                    button = tk.Button(frame, text=num, width=5, height=2, borderwidth=1, relief="solid", background='lightblue')
+                    button.config(command=lambda btn=button: select_day(btn))
+                    label_num = button
+                    num += 1
+                elif num <= num_dias:
+                    button = tk.Button(frame, text=num, width=5, height=2, borderwidth=1, relief="solid", background='white')
+                    button.config(command=lambda btn=button: select_day(btn))
+                    label_num = button
+                    num += 1
                 else:
-                    day_label = tk.Label(frame, text=day, width=4, height=2, borderwidth=1, relief="solid", anchor="center")
-                    day_label.bind("<Button-1>", lambda e, day=day: selecionar_dia(day))
-                day_label.grid(row=row, column=col, padx=2, pady=2)
-                # Destaque o dia atual (hoje)
-                if day == today and month_var.get() == datetime.datetime.now().month and year_var.get() == datetime.datetime.now().year:
-                    day_label.configure(bg='lightblue')
-                col += 1
+                    label_num = tk.Label(frame, text='', width=5, height=2, borderwidth=1, relief="solid", background='gray')
 
-    def clear_calendar():
-        for widget in frame.winfo_children():
-            widget.grid_forget()
+                label_num.grid(row=x, column=y)
 
-    
-
-    month_names = [calendar.month_name[i] for i in range(1, 13)]
-    day_names = ['Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb', 'Dom']
-
-    frame_main = tk.Frame(root,borderwidth=10,relief=tk.RIDGE)
-    frame_main.pack(expand=True,fill='both')
-    frame_calen = tk.Frame(frame_main,borderwidth=10,relief=tk.RIDGE)
-    frame_calen.pack()
-    frame_days = tk.Frame(frame_calen,borderwidth=10,relief=tk.RIDGE)
-    frame_days.pack()
-
-    selected_date = tk.StringVar()
-    header_frame = tk.Frame(frame_calen,borderwidth=10,relief=tk.RIDGE)
-    header_frame.pack()
-
-    prev_button = tk.Button(header_frame, text="Anterior", command=lambda: change_month(-1))
-    prev_button.grid(row=0, column=0)
-
-    month_var = tk.IntVar()
-    month_var.set(month)
-    month_option = tk.OptionMenu(header_frame, month_var, *range(1, 13))
-    month_option.grid(row=0, column=1)
 
     year_var = tk.IntVar()
     year_var.set(year)
-    year_entry = tk.Entry(header_frame, textvariable=year_var)
-    year_entry.grid(row=0, column=2)
+    month_var = tk.IntVar()
+    month_var.set(month)
 
-    next_button = tk.Button(header_frame, text="Próximo", command=lambda: change_month(1))
-    next_button.grid(row=0, column=3)
+    #main_frame = tk.Frame(windows,relief=tk.RIDGE,background='black')
+    #main_frame.pack(expand=True,fill='both')
 
-    frame = tk.Frame(frame_calen,borderwidth=10,relief=tk.RIDGE)
-    frame.pack()
 
-    selected_date_label = tk.Label(frame_main, textvariable=selected_date, font=("Helvetica", 12))
-    selected_date_label.pack()
+    frame = ttk.Frame(windows, width=400, height=400, relief=tk.RIDGE)
+    frame.grid_propagate(False)
+    frame.pack_propagate(False)
+    frame.pack(expand=True,fill='both')
 
-    def change_month(delta):
-        current_month = month_var.get()
-        new_month = current_month + delta
-        if new_month < 1:
-            new_month = 12
-            year_var.set(year_var.get() - 1)
-        elif new_month > 12:
-            new_month = 1
-            year_var.set(year_var.get() + 1)
-        month_var.set(new_month)
+    
+    frame.columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+    frame.rowconfigure(0, weight=0)
+    frame.rowconfigure((1, 2, 3, 4, 5, 6, 7, 8), weight=1)
 
-    month_var.trace("w", lambda *args: atualizar_calendario())
-    year_var.trace("w", lambda *args: atualizar_calendario())
+    months_pt = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
+    day_names = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
 
-    atualizar_calendario()
+    year_label = tk.Label(frame, text=year,font=('Helverica', 14))
+    year_label.grid(row=0,columnspan=7,sticky='nesw',padx=1,pady=1)
+    label_h = tk.Label(frame, text=months_pt[month-1],font=('Helverica', 14))
+    label_h.grid(row=1, columnspan=7, sticky='nesw',padx=1)
+
+
+    prev_button = tk.Button(frame, image=pastimg, borderwidth=0, command=lambda: change_month(-1))
+    prev_button.grid(row=1, column=0)
+
+    next_button = tk.Button(frame, image=nextimg, borderwidth=0, command=lambda: change_month(1))
+    next_button.grid(row=1, column=6)
+
+    col = 0
+    for dia in day_names:
+        label_dia = tk.Label(frame, text=dia, width=5, height=2, borderwidth=1, relief="solid", background='lightgray')
+        label_dia.grid(row=2, column=col)
+        col += 1
+
+    update_days()
+
+    style = ThemedStyle(windows)
+    style.set_theme('clam')
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    root.title('PetPlanner - Vacine seu pet')
-    root.minsize(1000,600)
-    #root.iconbitmap('logo.ico')
-    root_width = 1000
-    root_height = 700
-    display_width = root.winfo_screenwidth()
-    display_height = root.winfo_screenheight()
+    
+    windows = tk.Tk()
+    windows.title('Tabelas')
+    windows.geometry('600x600')
 
-    left = int(display_width / 2 - root_width / 2)
-    top = int(display_height / 2 - root_height / 2)
-    root.geometry(f'{root_width}x{root_height}+{left}+{top}')
+    nextimg = tk.PhotoImage(file='next.png')
+    pastimg = tk.PhotoImage(file='past.png')
+    calendario(windows,nextimg,pastimg)
 
-    month = 10
-    year = 2023 
-    mostrar_calendario(year, month,root)
+    windows.mainloop()
+    
 
-    root.mainloop()
+
--- petVacMain.py
+++ petVacMain.py
@@ -1,19 +1,22 @@
 import tkinter as tk
-from petPerfil import perfil
-from petCalendario import mostrar_calendario
-from petTela import tela_configs
+from petTela import Tela
+from petCalendario import calendario
+from petPerfil import Perfil
+
 
 #tela
 root = tk.Tk()
-tela_configs(root)
+config_perfil = Tela()
+config_perfil.tela_configs(root)
 
 #frame do perfil
-perfil(root)
+perfil = Perfil()
+perfil.criar_perfil(root)
 
 #frame do calendario
-month = 10
-year = 2023
-mostrar_calendario(year,month,root)
+nextimg = tk.PhotoImage(file='next.png')
+pastimg = tk.PhotoImage(file='past.png')
+calendario(root,nextimg,pastimg)
 
 #run
 root.mainloop()
