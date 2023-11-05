e729f0cc7acdf7433744d3dc6b2700bcdc341245
HenryMeloDev <pedrohenriquebmelo25@gmail.com>
2023-11-05 16:01:01 -0300

    Refatoração visual do app

--- petCalendario.py
+++ petCalendario.py
@@ -1,43 +1,79 @@
-import tkinter as tk
-from tkinter import ttk
 import calendar
 import datetime
-from ttkthemes import ThemedStyle
-
-year = datetime.datetime.now().year
-month = datetime.datetime.now().month
-day = datetime.datetime.now().day
-selected_day = None
+import customtkinter as ctk
+import tkinter as tk
+from typing import Optional, Tuple, Union
+from PIL import Image
 
 
-def calendario(windows,nextimg,pastimg):
+class Calendario(ctk.CTkFrame):
+    def __init__(self, master: any, width: int = 200, height: int = 200, corner_radius: Union[int, str, None] = None, border_width: Union[int, str, None] = None, bg_color: Union[str, Tuple[str, str]] = "transparent", fg_color: Union[str, Tuple[str, str], None] = None, border_color: Union[str, Tuple[str, str], None] = None, background_corner_colors: Union[Tuple[str, Tuple[str, str]], None] = None, overwrite_preferred_drawing_method: str | None = None, **kwargs):
+        super().__init__(master, width, height, corner_radius, border_width, bg_color, fg_color, border_color, background_corner_colors, overwrite_preferred_drawing_method, **kwargs)
+        self.year = datetime.datetime.now().year
+        self.month = datetime.datetime.now().month
+        self.day = datetime.datetime.now().day
+        self.selected_day = None
+        
+        self.columnconfigure((0,1,2), weight=1)
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)  
+        self.rowconfigure(2, weight=15)          
 
+        self.frame = ctk.CTkFrame(self, fg_color='#9156CD')  # Criar um Frame dentro do Calendario
+        self.frame.grid(column=0, row=2, sticky='nsew', columnspan=3, padx=20, pady=20)
+        
+        self.frame.columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
+        self.frame.rowconfigure(0, weight=0)
+        self.frame.rowconfigure((1, 2, 3, 4, 5, 6, 7, 8), weight=1)
 
-    def change_month(val):
-        global month
-        global year
-        month += val
-        if month < 1:
-            month = 12
-            year -= 1
-        elif month > 12:
-            month = 1
-            year += 1
-        label_h.config(text=months_pt[month-1])
-        year_label.config(text=year)
-        update_days()
+        # Substituir months_pt por uma lista válida de nomes dos meses em português
+        self.months_pt = [
+            "Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho",
+            "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"
+        ]
+        self.day_names = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
+        
+        prev_button = ctk.CTkButton(self, command=lambda: self.change_month(-1), fg_color='#402160',font=('Helverica', 15), width=30, height=55, corner_radius=120, text="<")
+        prev_button.grid(row=1, column=0)
 
-    def select_day(button):
-        global selected_day
-        if selected_day:
-            selected_day.config(bg='white')
-        selected_day = button
-        selected_day.config(bg='lightblue')
+        next_button = ctk.CTkButton(self, command=lambda: self.change_month(1),fg_color='#402160',font=('Helverica', 15),width=30, height=55,corner_radius=120, text=">")
+        next_button.grid(row=1, column=2)
 
-    def update_days():
+        col = 0
+        for dia in self.day_names:
+            label_dia = ctk.CTkLabel(self.frame, text=dia, width=50, height=50, fg_color='#402160', corner_radius=10,font=('Helverica', 20))
+            label_dia.grid(row=2, column=col, sticky='nsew', padx=5, pady=5)
+            col += 1
+        
+        year_label = ctk.CTkLabel(self, text=self.year, font=('Helverica', 24))
+        year_label.grid(column=1, row=0, pady=15)
+        label_h = ctk.CTkLabel(self, text=self.months_pt[self.month - 1], font=('Helverica', 24), fg_color='#402160', corner_radius=25)
+        label_h.grid(column=1, row=1, pady=5 , sticky="nsew")
+
+        self.update_days()
+
+    def change_month(self, val):
+        self.month += val
+        if self.month < 1:
+            self.month = 12
+            self.year -= 1
+        elif self.month > 12:
+            self.month = 1
+            self.year += 1
+        self.label_h.configure(text=self.months_pt[self.month-1])
+        self.year_label.configure(text=self.year)
+        self.update_days()
+
+    def select_day(self, button):
+        if self.selected_day:
+            self.selected_day.configure(bg='white')
+        self.selected_day = button
+        self.selected_day.configure(bg='lightblue')
+
+    def update_days(self):
         # Obter o número de dias no mês e o dia da semana do primeiro dia do mês
-        num_dias = calendar.monthrange(year, month)[1]
-        dia_semana = calendar.weekday(year, month, 1)
+        num_dias = calendar.monthrange(self.year, self.month)[1]
+        dia_semana = calendar.weekday(self.year, self.month, 1)
 
         # Ajustar para que domingo seja 0, segunda-feira seja 1, etc.
         dia_semana = (dia_semana + 1) % 7
@@ -47,80 +83,19 @@ def calendario(windows,nextimg,pastimg):
         for x in range(3, 9):
             for y in range(7):
                 if x == 3 and y < dia_semana:
-                    label_num = tk.Label(frame, text='', width=5, height=2, borderwidth=1, relief="solid", background='gray')
-                elif num == day and month == datetime.datetime.now().month and year == datetime.datetime.now().year:
-                    button = tk.Button(frame, text=num, width=5, height=2, borderwidth=1, relief="solid", background='lightblue')
-                    button.config(command=lambda btn=button: select_day(btn))
+                    label_num = ctk.CTkLabel(self.frame, text='', width=50, height=50, fg_color='#402160', corner_radius=10, font=('Helverica', 15))
+                elif num == self.day and self.month == datetime.datetime.now().month and self.year == datetime.datetime.now().year:
+                    button = ctk.CTkButton(self.frame, text=num, width=50, height=50, fg_color='#5E17EB', corner_radius=10, font=('Helverica', 15))
+                    button.configure(command=lambda btn=button: self.select_day(btn))
                     label_num = button
                     num += 1
                 elif num <= num_dias:
-                    button = tk.Button(frame, text=num, width=5, height=2, borderwidth=1, relief="solid", background='white')
-                    button.config(command=lambda btn=button: select_day(btn))
+                    button = ctk.CTkButton(self.frame, text=num, width=50, height=50, fg_color='#402160', corner_radius=10, font=('Helverica', 15))
+                    button.configure(command=lambda btn=button: self.select_day(btn))
                     label_num = button
                     num += 1
                 else:
-                    label_num = tk.Label(frame, text='', width=5, height=2, borderwidth=1, relief="solid", background='gray')
+                    label_num = ctk.CTkLabel(self.frame, text='', width=50, height=50, fg_color='#402160', corner_radius=10, font=('Helverica', 15))
 
-                label_num.grid(row=x, column=y)
-
-
-    year_var = tk.IntVar()
-    year_var.set(year)
-    month_var = tk.IntVar()
-    month_var.set(month)
-
-    #main_frame = tk.Frame(windows,relief=tk.RIDGE,background='black')
-    #main_frame.pack(expand=True,fill='both')
-
-
-    frame = ttk.Frame(windows, width=400, height=400, relief=tk.RIDGE)
-    frame.grid_propagate(False)
-    frame.pack_propagate(False)
-    frame.pack(expand=True,fill='both')
-
-    
-    frame.columnconfigure((0, 1, 2, 3, 4, 5, 6), weight=1)
-    frame.rowconfigure(0, weight=0)
-    frame.rowconfigure((1, 2, 3, 4, 5, 6, 7, 8), weight=1)
-
-    months_pt = ["Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro"]
-    day_names = ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb']
-
-    year_label = tk.Label(frame, text=year,font=('Helverica', 14))
-    year_label.grid(row=0,columnspan=7,sticky='nesw',padx=1,pady=1)
-    label_h = tk.Label(frame, text=months_pt[month-1],font=('Helverica', 14))
-    label_h.grid(row=1, columnspan=7, sticky='nesw',padx=1)
-
-
-    prev_button = tk.Button(frame, image=pastimg, borderwidth=0, command=lambda: change_month(-1))
-    prev_button.grid(row=1, column=0)
-
-    next_button = tk.Button(frame, image=nextimg, borderwidth=0, command=lambda: change_month(1))
-    next_button.grid(row=1, column=6)
-
-    col = 0
-    for dia in day_names:
-        label_dia = tk.Label(frame, text=dia, width=5, height=2, borderwidth=1, relief="solid", background='lightgray')
-        label_dia.grid(row=2, column=col)
-        col += 1
-
-    update_days()
-
-    style = ThemedStyle(windows)
-    style.set_theme('clam')
-
-
-if __name__ == '__main__':
-    
-    windows = tk.Tk()
-    windows.title('Tabelas')
-    windows.geometry('600x600')
-
-    nextimg = tk.PhotoImage(file='next.png')
-    pastimg = tk.PhotoImage(file='past.png')
-    calendario(windows,nextimg,pastimg)
-
-    windows.mainloop()
-    
-
+                label_num.grid(row=x, column=y, sticky='nsew', padx=10, pady=5)
 
--- petPerfil.py
+++ petPerfil.py
@@ -1,99 +1,54 @@
-import tkinter as tk
-from tkinter import ttk
-from ttkthemes import ThemedStyle
-from PIL import Image, ImageTk, ImageDraw
+import customtkinter
+from PIL import Image,ImageTk, ImageDraw
 
-class Perfil():
+class Perfil(customtkinter.CTkFrame):
+    def __init__(self, master, **kwargs):
+        super().__init__(master, **kwargs)
+        self.grid_propagate(True)
+        self.columnconfigure(0, weight=1)
+        self.rowconfigure((0,1,2), weight=1)
+        img = customtkinter.CTkFrame(master=self)
+        img.pack_propagate(False)
+        img.pack(side='left',fill='y',padx=2)
+        img.grid(row= 0, column = 0, pady = 15)
+        portrait = customtkinter.CTkImage(light_image=Image.open("img/jinx.jpg"),dark_image=Image.open("img/jinx.jpg"),size=(250, 250))
+        self.image = customtkinter.CTkLabel(img, image=portrait, text="", fg_color="#fff")
+        self.image.grid(row=0, column=0)
+        
 
-    def round_image(self,image_path, label):
-        # Carregar a imagem original
-        imagem_original = Image.open(image_path)
-        resized_image = imagem_original.resize((300,300))
+        infos = customtkinter.CTkFrame(master=self, width=250, height=200, fg_color="#402160")
+        infos.grid(row= 1, column = 0, sticky='ns')
+        infos.grid_propagate(False)
+        infos.columnconfigure((0,1),weight=1)
+        infos.rowconfigure((0,1,2,3,4),weight=1)
+        
+        
 
-        # Criar uma máscara circular
-        mascara = Image.new("L", (resized_image.width, resized_image.height), 0)
-        draw = ImageDraw.Draw(mascara)
-        draw.ellipse((0, 0, resized_image.width, resized_image.height), fill=255)
+        label_nome = customtkinter.CTkLabel(infos, text='Nome',font=('Helverica', 18))
+        entry_nome = customtkinter.CTkLabel(infos,text='Pepito',justify='right')
 
-        # Aplicar a máscara à imagem original
-        imagem_redonda = Image.new("RGBA", (resized_image.width, resized_image.height), (0, 0, 0, 0))
-        imagem_redonda.paste(resized_image, mask=mascara)
+        label_sexo = customtkinter.CTkLabel(infos,text='Sexo',font=('Helverica', 18))
+        entry_sexo = customtkinter.CTkLabel(infos,text='Macho',)
 
-        # Converter a imagem para o formato suportado pelo Tkinter
-        imagem_redonda_tk = ImageTk.PhotoImage(imagem_redonda)
+        label_raca = customtkinter.CTkLabel(infos,text='Raça',font=('Helverica', 18))
+        entry_raca = customtkinter.CTkLabel(infos,text='Gato preto')
 
-        # Exibir a imagem no label do portraid
-        label.config(image=imagem_redonda_tk)
-        label.image = imagem_redonda_tk  # Garantir que a referência persista
+        label_idade = customtkinter.CTkLabel(infos,text='Idade',font=('Helverica', 18))
+        entry_idade = customtkinter.CTkLabel(infos,text='3')
 
-    def criar_perfil(self,root):
+        label_peso = customtkinter.CTkLabel(infos,text='Peso',font=('Helverica', 18))
+        entry_peso = customtkinter.CTkLabel(infos,text='4')
 
-        div = ttk.Frame(root,width= 350, height=600, borderwidth=10,relief=tk.RIDGE)
-        div.pack_propagate(False)
-        div.pack(side='left',fill='y',padx=2)
-
-        portrait = tk.Label(div, background='gray')
-        portrait.pack(pady=10)
-        self.round_image("img/jinx.jpg", portrait)
-
-        frame_status = ttk.Frame(div,width= 250,height=200,borderwidth=10,relief=tk.RIDGE)
-        frame_status.grid_propagate(False)
-        frame_status.pack()
-
-        frame_status.columnconfigure((0,1),weight=1)
-        frame_status.rowconfigure((0,1,2,3,4),weight=1)
-
-        label_nome = ttk.Label(frame_status, text='Nome',font=('Helverica', 10))
-        entry_nome = ttk.Label(frame_status,text='Pepito',justify='right')
-
-        label_sexo = ttk.Label(frame_status,text='Sexo',font=('Helverica', 10))
-        entry_sexo = ttk.Label(frame_status,text='Macho',)
-
-        label_raca = ttk.Label(frame_status,text='Raça',font=('Helverica', 10))
-        entry_raca = ttk.Label(frame_status,text='Gato preto')
-
-        label_idade = ttk.Label(frame_status,text='Idade',font=('Helverica', 10))
-        entry_idade = ttk.Label(frame_status,text='3')
-
-        label_peso = ttk.Label(frame_status,text='Peso',font=('Helverica', 10))
-        entry_peso = ttk.Label(frame_status,text='4')
-
-        label_nome.grid(row=0,column=0,sticky='w')
+        label_nome.grid(row=0,column=0,sticky='w', padx = 15)
         entry_nome.grid(row=0,column=1,sticky='w')
-        label_sexo.grid(row=1,column=0,sticky='w')
+        label_sexo.grid(row=1,column=0,sticky='w', padx = 15)
         entry_sexo.grid(row=1,column=1,sticky='w')
-        label_raca.grid(row=2,column=0,sticky='w')
+        label_raca.grid(row=2,column=0,sticky='w', padx = 15)
         entry_raca.grid(row=2,column=1,sticky='w')
-        label_idade.grid(row=3,column=0,sticky='w')
+        label_idade.grid(row=3,column=0,sticky='w', padx = 15)
         entry_idade.grid(row=3,column=1,sticky='w')
-        label_peso.grid(row=4,column=0,sticky='w')
+        label_peso.grid(row=4,column=0,sticky='w', padx = 15)
         entry_peso.grid(row=4,column=1,sticky='w')
-
-        editButton = tk.Button(div, text='Cadastrar Vacinação', font=('Helverica', 10))
-        editButton.pack(pady=10)
-
-        #tema
-        style = ThemedStyle(root)
-        style.set_theme('clam')
-
-
-
-if __name__ == '__main__':
-
-    perfil = Perfil()
-    root = tk.Tk()
-    root.title('PetPlanner - Vacine seu pet')
-    root.minsize(1000,600)
-    #root.iconbitmap('logo.ico')
-    root_width = 1000
-    root_height = 700
-    display_width = root.winfo_screenwidth()
-    display_height = root.winfo_screenheight()
-
-    left = int(display_width / 2 - root_width / 2)
-    top = int(display_height / 2 - root_height / 2)
-    root.geometry(f'{root_width}x{root_height}+{left}+{top}')
-
-
-    perfil.criar_perfil(root)
-    root.mainloop()
+        
+        botão = customtkinter.CTkButton(self, text="Agendar", command='', fg_color="#402160", width=200, height=50, font=('Helverica', 18))
+        botão.grid(row=2, column=0)
